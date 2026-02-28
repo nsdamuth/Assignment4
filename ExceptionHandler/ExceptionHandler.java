1e286a1e2b5ddf963deb367ad8d21ba5d53f5fac
Nick Damuth <nick@cotasystems.com>
2026-02-27 17:02:53 -0800

    Example alternative exception

--- ExceptionHandler/ExceptionHandler.java
+++ ExceptionHandler/ExceptionHandler.java
@@ -1,57 +1,52 @@
-/* 
- Exception Handler
- Author: Nicholas S. Damuth
- School of Technology and Engineering, National University
- CSC262: Programming in JAVA
- Artee Dubey
- February 24, 2026
-*/
-// package ExceptionHandler;
+// package assignment4;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
-// import Banner.Banner;
-// Moved Banner back to this file instead of its own package to 
-// keep things easier for review and sharing
-class Banner {
-    final static int WINDOW_LENGTH = 100;
-    // Retained here rather than abstracted into its own package for the purpose 
-    // of keeping all the code in a single file
-    private static String _framed_center(String text, int width, String border) {
-        border = (border != null) ? border : "*";
-        int inner = width - 2;
-        int padding = inner - text.length();
-        int left = padding / 2;
-        int right = padding - left;
-
-        return border + " ".repeat(left) + text + " ".repeat(right) + border;
-    }
-    public static String _auto_frame(String banner) {
-        return _framed_center(banner, WINDOW_LENGTH, null);
-    }
-    public void print(String name) {
-        // Generic banner for class
-        String[] banners = {name, "Nicholas S. Damuth", "School of Technology and Engineering, National University", "CSC262: Programming in JAVA", "Artee Dubey", "February 24, 2026"};
-        System.out.printf("%s%n", "*".repeat(WINDOW_LENGTH));
-        for (String banner : banners) {
-            System.out.println(_auto_frame(banner));
-        }
-        String[] files = new String[3];
-        System.out.printf("%s%n", "*".repeat(WINDOW_LENGTH));
-    }
-}
-
 public class ExceptionHandler {
-    // Keeping this here and simple rather than an err-logging class 
-    // in its own package for simplicity sake similar to banner
-    private static void _handle_err(String log_msg, String method) {
-        long threadId = Thread.currentThread().threadId();
-        System.out.println(String.format("%s - method : %s [%s]", log_msg, method, threadId));
-    }
     public static void main(String[] args) {
-        Banner banner = new Banner();
-        banner.print("Exception Handler"); 
-        
-        Scanner scanner = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter file name: ");
+        String filename = in.next();
+        in.close();
+
+        Scanner file_data = null;
+        Integer numberOfValues = 0;
+        while (file_data == null) {
+            try {
+                File inFile = new File(filename);
+                file_data = new Scanner(inFile);
+                numberOfValues = file_data.nextInt();
+            }
+            catch (FileNotFoundException exception){
+                System.out.println("File not found");
+            }
+            catch (NoSuchElementException exception){
+                System.out.println("File contents invalid");
+            }
+
+
+            if (numberOfValues != null) {
+                Double sum = null;
+                for (int i = 0; i < numberOfValues; i++) {
+                    try {
+                        sum = ((sum == null) ? 0 : sum) + file_data.nextDouble();
+                    } catch (Exception e) {
+                        System.out.println("Failed to convert value");
+                        sum = null;
+                        break;
+                    }
+                }
+                
+                if (sum != null) {
+                    System.out.println(String.format("The sum is %s", sum));
+                }
+            }
+        }
     }
+
 }
